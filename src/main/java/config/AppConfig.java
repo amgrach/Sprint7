bf1ecab804843f3dc6b82bb2954332c696a8efff
amgrach <amgrach@gmail.com>
2023-10-11 14:42:58 +0300

    Orders Tests Commit

--- src/main/java/config/AppConfig.java
+++ src/main/java/config/AppConfig.java
@@ -4,5 +4,7 @@ public class AppConfig {
     public final static String APP_URL = "https://qa-scooter.praktikum-services.ru/";
     public static final String COURIER_URL = "api/v1/courier/";
     public static final String LOGIN_URL = "api/v1/courier/login/";
+    public static final String ORDER_URL = "api/v1/orders/";
+    public static final String CANCEL_ORDER_URL = "api/v1/orders/cancel/";
 
 }
